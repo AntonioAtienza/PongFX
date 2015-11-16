eb20f3ce13c57377e31712c53916fbe81649bfda
1DAW01 <1DAW01@192.168.201.59>
2015-11-16 14:55:30 +0100

    Initial commit

--- src/pongfx/PongFX.java
+++ src/pongfx/PongFX.java
@@ -10,6 +10,7 @@ import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
 import static javafx.scene.paint.Color.BLACK;
@@ -20,18 +21,13 @@ import javafx.stage.Stage;
 
 /**
  *
- * @author 1DAW01
+ * @Antonio Rojo Atienza
  */
 public class PongFX extends Application {
     
     final int WORLD_WIDTH = 400;
     final int WORLD_HEIGHT = 250;
     
-    //Datos sobre la barra
-    //final int BAR_WIDTH = ;
-    //int barPosXPlayer2 = 0;
-    //int barPosY = 0;
-    //final int BAR_SPEED = 8;
     int gravityPlayer1 = 2;
     int gravityPlayer2 = 2;
     final int BAR_HEIGHT = 50;
@@ -41,6 +37,8 @@ public class PongFX extends Application {
     int dirYBall = 2;
     int posBall= 0;
     int posBar2 = 0;
+    int posXPlayer2 = WORLD_WIDTH - 10;
+    
     @Override
     public void start(Stage primaryStage) {
         Group root = new Group();
@@ -50,67 +48,84 @@ public class PongFX extends Application {
         primaryStage.show();
         
         Rectangle barPlayer1 = new Rectangle(5, BAR_HEIGHT, BLACK);
-        root.getChildren().add(barPlayer1);
         barPlayer1.setTranslateX(5);
+        root.getChildren().add(barPlayer1);
         
         Rectangle barPlayer2 = new Rectangle(5, BAR_HEIGHT, BLACK);
         barPlayer2.setTranslateX(390);
         root.getChildren().add(barPlayer2);
         
-        
         Circle ball = new Circle(RADIO_BALL,BROWN);
-        ball.setTranslateX (WORLD_WIDTH/2);
-        ball.setTranslateY(WORLD_HEIGHT/2);
+        ball.setTranslateX (WORLD_WIDTH*0.5);
+        ball.setTranslateY(WORLD_HEIGHT*0.5);
         root.getChildren().add(ball);
+        
+        Rectangle line = new Rectangle(0.5,WORLD_HEIGHT, BLACK);
+        line.setTranslateX(WORLD_WIDTH*0.5);
+        root.getChildren().add(line);
+        
+        Label marcador1 = new Label(String.valueOf(0));
+        marcador1.setTranslateX(WORLD_WIDTH*0.35);
+        root.getChildren().add(marcador1);
+        
+        Label marcador2 = new Label(String.valueOf(0));
+        marcador2.setTranslateX(WORLD_WIDTH*0.65 - 10);
+        root.getChildren().add(marcador2);
+        
         
         new AnimationTimer() {
             @Override
             public void handle(long now) {
-                
-                double posX = ball.getTranslateX();
-                ball.setTranslateX(posX + dirXBall);
-                //System.out.println(posX);
-                if(posX == WORLD_WIDTH-RADIO_BALL ){
+                //Movimiento de la bola en el eje X
+                double posXBall = ball.getTranslateX();
+                ball.setTranslateX(posXBall + dirXBall);
+                //System.out.println(posXBall);
+                if(posXBall == WORLD_WIDTH - RADIO_BALL ){
                     dirXBall = -3;
                 }
-                if(posX == RADIO_BALL){
+                if(posXBall == RADIO_BALL){
                     dirXBall = +3;
                 }
-                double posY = ball.getTranslateY();
-                ball.setTranslateY (posY + dirYBall);
-                //System.out.println(posY);
-                if(posY == WORLD_HEIGHT-RADIO_BALL){
+                //Movimiento de la bola en el eje de coordenadas Y
+                double posYBall = ball.getTranslateY();
+                ball.setTranslateY (posYBall + dirYBall);
+                //System.out.println(posYBall);
+                if(posYBall == WORLD_HEIGHT - RADIO_BALL){
                     dirYBall = -2;
                 }
-                if(posY == RADIO_BALL){
+                if(posYBall == RADIO_BALL){
                     dirYBall = +2;
                 }
                 
+                //Movimiento de la pala del jugador 1
                 double posYPlayer1 = barPlayer1.getTranslateY();
                 posYPlayer1 += gravityPlayer1;
                 barPlayer1.setTranslateY(posYPlayer1);
                 //System.out.println(posYPlayer1);
-                
                 if(posYPlayer1 <= 0){
-                    gravityPlayer1 = 0;
+                    barPlayer1.setTranslateY(0);
                 }
-                if(posYPlayer1 >= WORLD_HEIGHT-BAR_HEIGHT){
-                    gravityPlayer1 = 0;
+                if(posYPlayer1 >= WORLD_HEIGHT - BAR_HEIGHT){
+                    barPlayer1.setTranslateY(WORLD_HEIGHT - BAR_HEIGHT);
                 }
                 
+                 //Movimiento de la pala del jugador 2
                 double posYPlayer2 = barPlayer2.getTranslateY();
                 posYPlayer2 += gravityPlayer2;
                 barPlayer2.setTranslateY(posYPlayer2);
-                
+                //System.out.println(posYPlayer2);
                 if(posYPlayer2 <= 0){
-                    gravityPlayer2 = 0;
+                    barPlayer2.setTranslateY(0);
                 }
-                if(posYPlayer2 >= WORLD_HEIGHT-BAR_HEIGHT){
-                    gravityPlayer2 = 0;
+                if(posYPlayer2 >= WORLD_HEIGHT - BAR_HEIGHT){
+                    barPlayer2.setTranslateY(WORLD_HEIGHT - BAR_HEIGHT);
                 }
-                /*if(posX == 390 && posYPlayer2 == 390){
-                    dirXBall = -3;
-                }*/
+                if(posXBall == posXPlayer2){
+                    if (posYBall >= posYPlayer2 && posYBall <= (posYPlayer2 + BAR_HEIGHT)){
+                            dirXBall = -3;
+                    }
+                         
+                }
             }
         }.start();
         
